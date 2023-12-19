b06035c3f5d23aa394b7b4bc6a7a0d08ad27465f
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-12-19 13:26:52 -0300

    refactor: UserRepository removed http status

--- src/repositories/UserRepository.php
+++ src/repositories/UserRepository.php
@@ -14,109 +14,70 @@ class UserRepository implements Repository
 
     public static function selectAll()
     {
-        $conn = DatabaseConnection::getConnection();
-
+        $conn = null;
         try {
-
+            $conn = DatabaseConnection::getConnection();
             $query = "SELECT * FROM users";
             $stmt = $conn->prepare($query);
             $stmt->execute();
 
-            if ($stmt->rowCount() > 0) {
-
-                http_response_code(200);
+            if ($stmt->rowCount() > 0) {                
                 return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            } else {
-
-                http_response_code(404);
-                echo json_encode(['message' => 'Users not found']);
-
             }
-
         } catch (Exception $e) {
-
             http_response_code(500);
             echo json_encode(['message' => $e->getMessage()]);
-
         } finally {
-
             $conn = null;
-
         }
     }
 
     public static function selectById(int $id)
     {
-        $conn = DatabaseConnection::getConnection();
-
+        $conn = null;
         try {
-
+            $conn = DatabaseConnection::getConnection();            
             $query = "SELECT * FROM users WHERE id = ? LIMIT 1";
             $stmt = $conn->prepare($query);
             $stmt->execute(array($id));
 
             if ($stmt->rowCount() > 0) {                
                 return $stmt->fetch(PDO::FETCH_ASSOC);
-            } else {
-
-                http_response_code(404);
-                echo json_encode(['message' => 'User not found']);
-
             }
-
         } catch (Exception $e) {
-
             http_response_code(500);
             echo json_encode(['message' => $e->getMessage()]);
-
         } finally {
-
             $conn = null;
-
         }
     }
 
     public static function selectByEmail(string $email)
     {
-        $conn = DatabaseConnection::getConnection();
-
+        $conn = null;        
         try {
-
+            $conn = DatabaseConnection::getConnection();
             $query = "SELECT * FROM users WHERE email = ? LIMIT 1";
             $stmt = $conn->prepare($query);
             $stmt->execute(array($email));
 
-            if ($stmt->rowCount() > 0) {
-
-                http_response_code(200);
+            if ($stmt->rowCount() > 0) {                
                 return $stmt->fetch(PDO::FETCH_ASSOC);
-
-            } else {
-
-                http_response_code(404);
-                echo json_encode(['message' => 'User email not found']);
-
             }
 
         } catch (Exception $e) {
-
             http_response_code(500);
             echo json_encode(['message' => $e->getMessage()]);
-
         } finally {
-
             $conn = null;
-
         }
     }
 
     public static function insert(object $user)
     {
-        $conn = DatabaseConnection::getConnection();
-
+        $conn = null; 
         try {
-            
+            $conn = DatabaseConnection::getConnection();            
             $conn->beginTransaction();
             $query = "INSERT INTO users (`full_name`,`cpf`,`cnpj`,`email`,`password`,`type`) 
                       VALUES (?, ?, ?, ?, ?, ?)";
@@ -131,29 +92,21 @@ class UserRepository implements Repository
                 $user->getType()
             ));
 
-            $conn->commit();
-            http_response_code(201);
-            echo json_encode(['message' => 'User created successfully']);
-
+            $conn->commit();  
         } catch (Exception $e) {
-
             $conn->rollBack();
             http_response_code(500);
             echo json_encode(['message' => $e->getMessage()]);
-
         } finally {
-
             $conn = null;
-
         }
     }
 
     public static function update(object $user)
     {
-        $conn = DatabaseConnection::getConnection();
-
+        $conn = null;
         try {
-
+            $conn = DatabaseConnection::getConnection();
             $conn->beginTransaction();
             $query = "UPDATE users SET `full_name` = ?, 
                                        `cpf` = ?, 
@@ -174,49 +127,32 @@ class UserRepository implements Repository
                 $user->getId()
             ));
 
-            $conn->commit();
-
-            http_response_code(200);
-            echo json_encode(['message' => 'User updated successfully']);
-
+            $conn->commit(); 
         } catch (Exception $e) {
-
             $conn->rollBack();
             http_response_code(500);
             echo json_encode(['message' => $e->getMessage()]);
-
         } finally {
-
             $conn = null;
-
         }
     }
 
     public static function deleteById(int $id)
     {
-        $conn = DatabaseConnection::getConnection();
-
+        $conn = null;
         try {
-
+            $conn = DatabaseConnection::getConnection();
             $conn->beginTransaction();
             $query = "DELETE FROM users WHERE id = ?";
             $stmt = $conn->prepare($query);
             $stmt->execute(array($id));
-            $conn->commit();
-
-            http_response_code(204);
-            echo json_encode(['message' => 'User deleted successfully']);
-
+            $conn->commit(); 
         } catch (Exception $e) {
-
             $conn->rollBack();
             http_response_code(500);
             echo json_encode(['message' => $e->getMessage()]);
-
         } finally {
-
             $conn = null;
-
         }
     }
 }
