9570d19a667a2142014bec4f0568625819537213
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-12-21 21:25:16 -0300

    update: Routes api, add /transactions route

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,8 +14,9 @@ Router::get('   /api/v1/users/{id}',  'UserController@selectOne');
 Router::post('  /api/v1/users',       'UserController@insert');
 Router::put('   /api/v1/users/{id}',  'UserController@update');
 Router::delete('/api/v1/users/{id}',  'UserController@delete');
+// Router::post('  /api/v1/users/deposit', 'UserController@deposit');
 
 // ********************************************************************************************
 // ********************************************************************************************
 
-Router::get('  /api/v1/transactions/',  'TransactionController@selectAll');
+Router::get('  /api/v1/transactions',  'TransactionController@selectAll');
