92b266193df4a2b1c6fe8eec0429fb5cb0abb9b5
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-12-27 15:41:58 -0300

    refactor: Response class clean

--- src/app/http/Response.php
+++ src/app/http/Response.php
@@ -11,35 +11,33 @@ class Response
 
     public static function json(array $message = [], int $statusCode = 200): void
     {
-        if (!empty($message)) {
-
-            $message['statusCode'] = $statusCode;
+        if (!empty($message)) {            
             
             switch ($statusCode) { 
                 case 201:
                     $message['success'] = 'Created';
+                    $message['statusCode'] = 201;
                     break;
                 case 400:
                     $message['error'] = 'Bad Request';
+                    $message['statusCode'] = 400;
+                    break;                
+                case 404:
+                    $message['error'] = 'Not Found'; 
+                    $message['statusCode'] = 404; 
                     break;                
                 case 409:
                     $message['error'] = 'Conflict';
+                    $message['statusCode'] = 409;
                     break;                
                 case 500:
                     $message['error'] = 'Internal Server Error';
+                    $message['statusCode'] = 500;
                     break;
                 case 503:
                     $message['error'] = 'Service Unavailable';
+                    $message['statusCode'] = 503;
                     break;                
-                case 404:
-                    $message['error'] = 'Not Found';  
-                    break;
-                case 500:
-                    $message['error'] = 'Internal Server Error';
-                    break; 
-                default:
-                    $message['success'] = 'OK';
-                    break;  
             }           
 
             header('Content-Type: application/json');
