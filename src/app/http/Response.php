f0014f83ca2af2a6c43cf638f37bb93bee054ed3
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-12-27 14:49:03 -0300

    feat: Response json with most used status code

--- src/app/http/Response.php
+++ src/app/http/Response.php
@@ -9,12 +9,40 @@ class Response
     // ********************************************************************************************
     // ********************************************************************************************
 
-    public static function json(array $data = []): void
+    public static function json(array $message = [], int $statusCode = 200): void
     {
-        if (!empty($data)) {
+        if (!empty($message)) {
+            
+            switch ($statusCode) { 
+                case 201:
+                    $message['success'] = 'Created';    
+                    break;
+                case 400:
+                    $message['error'] = 'Bad Request';
+                    break;                
+                case 409:
+                    $message['error'] = 'Conflict';
+                    break;                
+                case 500:
+                    $message['error'] = 'Internal Server Error';
+                    break;
+                case 503:
+                    $message['error'] = 'Service Unavailable';
+                    break;                
+                case 404:
+                    $message['error'] = 'Not Found';  
+                    break;
+                case 500:
+                    $message['error'] = 'Internal Server Error';
+                    break; 
+                default:
+                    $message['success'] = 'OK';
+                    break;  
+            }
+
             header('Content-Type: application/json');
-            http_response_code($data['statusCode'] ?? 200);
-            echo json_encode($data);
+            http_response_code($statusCode);
+            echo json_encode($message);
             exit;
         }
     }
