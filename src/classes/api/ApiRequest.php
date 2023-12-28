803b6a200c3631bfab0af27deadaadcc9bb4e337
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-12-28 16:14:13 -0300

    refactor: ApiRequest change message to response in json

--- src/classes/api/ApiRequest.php
+++ src/classes/api/ApiRequest.php
@@ -1,31 +1,36 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 namespace src\classes\api;
 
 use src\app\http\Response;
+use Exception;
 
 class ApiRequest
 {
 
     public static function get(string $url = '')
     {
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        try {
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-        curl_close($curl);
+            curl_close($curl);
 
-        if ($response !== false) {            
-            if ($httpCode >= 200 && $httpCode < 300) {
-                return json_decode($response, true);
+            if ($response !== false) {
+                if ($httpCode >= 200 && $httpCode < 300) {
+                    return json_decode($response, true);
+                } else {
+                    Response::json(['message' => 'API request error', 'api_url' => $url], $httpCode);
+                }
             } else {
-                Response::json(['response' => 'Api request error'], $httpCode);                
+                Response::json(['message' => 'API request error', 'api_url' => $url], $httpCode);
             }
-        } else {
-            Response::json(['response' => 'Api request error'], $httpCode); 
+        } catch (Exception $e) {
+            Response::json(['message' => $e->getMessage(), 'api_url' => $url], 500);
         }
     }
 }
