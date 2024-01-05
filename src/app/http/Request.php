b1efb73d4f5e0d4fd3b8b1300d7ef0b98f1c7ddf
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2024-01-04 22:15:34 -0300

    docs: Request class

--- src/app/http/Request.php
+++ src/app/http/Request.php
@@ -9,32 +9,57 @@ class Request
     private array $urlParams;
     private array $requestParams;
 
-    // ********************************************************************************************
-    // ********************************************************************************************
+    /* ************************************************************************ 
+     *
+     * Retrieves the value of a path parameter.
+     *
+     * @param string $param The name of the path parameter.
+     * @return string The value of the path parameter.   
+     *  
+     * ***********************************************************************/
 
     public function PathParam(string $param = ''): string
     {
         return $this->urlParams[$param];
     }
 
-    // ********************************************************************************************
-    // ********************************************************************************************
+    /* ************************************************************************ 
+     * 
+     * Sets the URL parameters for the current object.
+     *
+     * @param array $urlParams The URL parameters to be set.
+     * @return void     
+     *  
+     * ***********************************************************************/
 
     public function setParamsFromURL(array $urlParams): void
     {
         $this->urlParams = $urlParams;
     }
 
-    // ********************************************************************************************
-    // ********************************************************************************************
+    /* ************************************************************************ 
+     * 
+     * Sets the parameters from the request.
+     *
+     * @param array $requestParams The request parameters.
+     * @return void
+     *  
+     * ***********************************************************************/
 
     public function setParamsFromRequest(array $requestParams): void
     {
         $this->requestParams = $requestParams;
     }
 
-    // ********************************************************************************************
-    // ********************************************************************************************
+    /* ************************************************************************ 
+     * 
+     * Retrieves the value of a specified parameter from the request.
+     *
+     * @param string $param The name of the parameter to retrieve. 
+     * Default is an empty string.     
+     * @return mixed The value of the specified parameter.
+     *  
+     * ***********************************************************************/
 
     public function getParam(string $param = '')
     {
@@ -42,9 +67,6 @@ class Request
             Response::json(['message' => "{$param} is required"], 400);
         }
 
-        return $this->requestParams[$param];            
+        return $this->requestParams[$param];
     }
-
-    // ********************************************************************************************
-    // ********************************************************************************************
 }
