2c7fc6be85fb4db1e53efdbe99ece8ff5048deb7
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2024-01-05 22:08:35 -0300

    docs: ApplicationProperties class

--- src/classes/util/ApplicationProperties.php
+++ src/classes/util/ApplicationProperties.php
@@ -8,6 +8,17 @@ use src\app\http\Response;
 
 class ApplicationProperties
 {
+    /**************************************************************************
+     * Retrieves a value from the application properties file based 
+     * on the given key.
+     *
+     * @param string $key The key to look up in the application properties file. 
+     * Defaults to an empty string.
+     * 
+     * @return mixed The value corresponding to the given key in the 
+     * application properties file, or null if the key is not found.
+     *************************************************************************/
+    
     public static function get(string $key = '')
     {
         $properties = parse_ini_file(__DIR__ . "/../../../src/application.properties");
