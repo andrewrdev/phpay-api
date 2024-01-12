bb631742c053af9306ed2193fc6874d0aa6084f1
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2024-01-12 10:16:57 -0300

    docs: Validation

--- src/validations/Validation.php
+++ src/validations/Validation.php
@@ -8,7 +8,17 @@ use src\app\http\Response;
 
 class Validation
 {
-    public static function validateID(int|null $id, string $message = 'id is invalid')
+    /**************************************************************************
+     * Validates the given ID.
+     *
+     * @param int|null $id The ID to validate.
+     * @param string $message The message to display if the ID is invalid. 
+     * Default is 'id is invalid'.
+     *      
+     * @return void
+     *************************************************************************/
+
+    public static function validateID(?int $id, string $message = 'id is invalid')
     {
         if ($id !== null) {
             if (!filter_var($id, FILTER_VALIDATE_INT) || $id < 1) {
